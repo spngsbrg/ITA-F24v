c8e8760ea28db7830e40318c94dbeb874a3175a7
Thomas Spangsberg <thsp@eaaa.dk>
2024-05-17 09:20:32 +0200

    updtaed greeting

--- GitHubStuff/script.js
+++ GitHubStuff/script.js
@@ -2,4 +2,4 @@ console.log("success");
 
 let start = document.getElementById("beginning");
 
-start.innerHTML = "<h1>Welcome</h1> <p>Hello</p>";
+start.innerHTML = "<h1>Welcome</h1> <p>Hello to you</p>";
